01d424a0b1d10d4b7b64ad41ec1beece2f20bcdd
Aleksandr Razin <razin.x.aleks@gmail.com>
2023-11-05 23:48:01 +0300

    deleted:    Interpretable_ML/src/utils/helpers.py 	deleted:    Interpretable_ML/src/utils/plots.py 	deleted:    Interpretable_ML/src/visualization/OfflineAnomalyVisualizer.py 	deleted:    Interpretable_ML/src/visualization/__init__.py

--- Interpretable_ML/src/utils/helpers.py
+++
@@ -1,6 +0,0 @@
-# Decorator for making the TS have only relevant features (that we used in anomalies research)
-def preprocess_decorator(func):
-    def wrapper(self, *args, **kwargs):
-        df = func(self, *args, **kwargs)
-        return self.preprocess_data(df)
-    return wrapper
